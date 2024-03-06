56aa49c23fec1fd0a2a1f07f622f6471a20a03de
bashabi <bashabi@themonsoon.com>
2024-03-06 14:27:31 +0000

    Initial Commit and started home view

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require '../helpers.php';
+
+require basePath('views/home.view.php');     // views/home.view.php  - is the value of $path passed as an argument for basPath() in to the helper.php file 
