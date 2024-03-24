5b91fa00a2ab655f7cdfc3c5fbcb6d3e96a6d3f0
bashabi <bashabi@themonsoon.com>
2024-03-24 12:55:09 +0000

    Edit Form and Update Functionality

--- APP/controllers/ListingController.php
+++ APP/controllers/ListingController.php
@@ -144,4 +144,100 @@ class ListingController
         $_SESSION['success_message'] = 'Listing Deleted Successfully';
         redirect('/listings');
     }
+
+
+    /**
+     * show the listing edit form
+     * 
+     * @params 
+     * 
+     */
+
+    public function edit($params)
+    {
+        $id = $params['id'] ?? '';
+        $params = [
+            'id' => $id,
+        ];
+
+        $listing  = $this->db->query('SELECT * FROM listings where id = :id', $params)->fetch();
+        // inspect($listing);
+
+        if (!$listing) {
+            ErrorController::notFound('Listing Not found');
+            return;
+        }
+
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * UPDATE a Listing
+     * 
+     * 
+     * @param array $params
+     * @return void
+     * 
+     */
+
+    public function update($params)
+    {
+        $id = $params['id'] ?? '';
+        $params = [
+            'id' => $id,
+        ];
+
+        $listing  = $this->db->query('SELECT * FROM listings where id = :id', $params)->fetch();
+        // inspect($listing);
+
+        if (!$listing) {
+            ErrorController::notFound('Listing Not found');
+            return;
+        }
+
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+        $updatedValues = [];
+        $updatedValues = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $updatedValues = array_map('sanitize', $updatedValues);
+
+        $requiredFields = ['title', 'description', 'email', 'city'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($updatedValues[$field]) || !Validation::string($updatedValues[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView('listings/edit', [
+                'listing' => $listing,
+                'errors' => $errors
+
+            ]);
+            exit;
+        } else {
+            //submit to the database
+            $updatedFields = [];
+            foreach (array_keys($updatedValues) as $field) {
+                $updatedFields[] = "{$field} = :{$field}";
+            }
+
+            $updatedFields = implode(', ', $updatedFields);
+
+            $updateQuery = "UPDATE listings SET $updatedFields where id = :id";
+            $updatedValues['id'] = $id;
+
+            $this->db->query($updateQuery, $updatedValues);
+
+            $_SESSION['success_message'] = 'Listing Updated successfully';
+
+            redirect('/listings/' . $id);
+        }
+    }
 }
--- APP/views/listings/show.view.php
+++ APP/views/listings/show.view.php
@@ -3,6 +3,8 @@
 <?php loadPartials('topbanner'); ?>
 
 <section class="container mx-auto p-4 mt-4">
+    <?php loadPartials('message'); ?>
+
     <div class="rounded-lg shadow-md bg-white p-3">
         <div class="flex justify-between items-center">
             <a class="block p-4 text-blue-700" href="/listings">
@@ -10,7 +12,7 @@
                 Back To Listings
             </a>
             <div class="flex space-x-4 ml-4">
-                <a href="/edit" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+                <a href="/listings/edit/<?= $listing->id ?>" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
                 <!-- Delete Form -->
                 <form method="POST">
                     <input type="hidden" name="_method" value="DELETE">
