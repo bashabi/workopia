17f739292ec6bf08123fffdd2791fbca63537de3
bashabi <bashabi@themonsoon.com>
2024-03-24 15:08:21 +0000

    fixed error on navbar partial

--- APP/views/partials/navbar.php
+++ APP/views/partials/navbar.php
@@ -17,9 +17,10 @@ use Framework\Session;
                         <button type="submit" class="text-white inline hover:underline">Logout</button>
                     </form>
                 </div>
+            <?php } else { ?>
+                <a href="/auth/login" class="text-white hover:underline">Login</a>
+                <a href="/auth/register" class="text-white hover:underline">Register</a>
             <?php } ?>
-            <a href="/auth/login" class="text-white hover:underline">Login</a>
-            <a href="/auth/register" class="text-white hover:underline">Register</a>
             <a href="/listings/create" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
         </nav>
     </div>
