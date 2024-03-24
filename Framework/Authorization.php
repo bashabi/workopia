03a0a16586e3ec97998f84b434f0ca482c713b03
bashabi <bashabi@themonsoon.com>
2024-03-24 17:20:59 +0000

    Delete listing with Authorization

---
+++ Framework/Authorization.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Framework;
+
+use Framework\Session;
+
+class Authorization
+{
+
+    /**
+     * Check if the current user own a resource
+     * 
+     * @param int $resourceId
+     * @return bool
+     */
+
+    public static function isOwner($resourceId)
+    {
+
+        $sessionUser = Session::get('user');
+
+        if ($sessionUser !== null && isset($sessionUser['id'])) {
+            $sessionUserId = (int) $sessionUser['id'];
+            return $sessionUserId  === $resourceId;
+        }
+
+        return false;
+    }
+}
